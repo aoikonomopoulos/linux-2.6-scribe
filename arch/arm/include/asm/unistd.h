99c36673b75fb61c109c7599fbc2b57e55a7ef69
Angelos Oikonomopoulos <a.oikonomopoulos@vu.nl>
2012-03-05 16:15:29 +0100

    define {g,s}et_scribe_flags on arm

--- arch/arm/include/asm/unistd.h
+++ arch/arm/include/asm/unistd.h
@@ -393,6 +393,8 @@
 #define __NR_perf_event_open		(__NR_SYSCALL_BASE+364)
 #define __NR_recvmmsg			(__NR_SYSCALL_BASE+365)
 #define __NR_eclone			(__NR_SYSCALL_BASE+366)
+#define __NR_get_scribe_flags		(__NR_SYSCALL_BASE+367)
+#define __NR_set_scribe_flags		(__NR_SYSCALL_BASE+368)
 
 /*
  * The following SWIs are ARM private.
